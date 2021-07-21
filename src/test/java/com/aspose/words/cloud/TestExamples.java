d8ae6d7f77e682ae311965903100cedb2b071d53
evgeny.ivanov <evgeny.ivanov>
2021-07-21 07:35:25 -0500

    SDK regenerated by CI server [ci skip]

--- src/test/java/com/aspose/words/cloud/TestExamples.java
+++ src/test/java/com/aspose/words/cloud/TestExamples.java
@@ -81,19 +81,4 @@ public class TestExamples extends TestCase
         AcceptAllRevisionsOnlineResponse acceptAllRevisionsOnlineResult = wordsApi.acceptAllRevisionsOnline(requestRequest);
         Files.write(Paths.get("test_result.docx"), acceptAllRevisionsOnlineResult.getDocument());
     }
-
-    @Test
-    public void testUpdateBookmark() throws Exception
-    {
-        WordsApi wordsApi = new WordsApi(apiClient);
-        String remoteFileName = "Sample.docx";
-        String bookmarkName = "aspose";
-
-        BookmarkData testBookmarkData = new BookmarkData();
-        testBookmarkData.setName(bookmarkName);
-        testBookmarkData.setText("New Bookmark Text");
-
-        UpdateBookmarkRequest updateBookmarkRequest = new UpdateBookmarkRequest(remoteFileName,bookmarkName,testBookmarkData,null,null,null,null,null,null,null);
-        wordsApi.updateBookmark(updateBookmarkRequest);
-    }
 }
