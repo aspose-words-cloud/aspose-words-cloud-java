e9acc896e42aaacd36932621235d2335cf09614d
evgeny.ivanov <evgeny.ivanov>
2021-08-16 11:01:47 -0500

    SDK regenerated by CI server [ci skip]

--- src/main/java/com/aspose/words/cloud/ApiClient.java
+++ src/main/java/com/aspose/words/cloud/ApiClient.java
@@ -56,7 +56,7 @@ public class ApiClient {
     private String apiVersion = "v4.0";
     private String baseUrl = "https://api.aspose.cloud";
     private String basePath = baseUrl + "/" + apiVersion;
-    private String clientVersion = "21.7";
+    private String clientVersion = "21.8";
     private boolean debugging = false;
     private Map<String, String> defaultHeaderMap = new HashMap<String, String>();
     private String tempFolderPath = null;
